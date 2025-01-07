5e81795c9a29de65d788f655c9485c0166747729
Peninah98 <peninangizwenayo@gmail.com>
2025-01-07 11:27:29 +0200

    chore: create the  new project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { LinearGradient } from "expo-linear-gradient"
 
-export default function App() {
+const APP = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <LinearGradient
+      colors={["#bb52aa", "#63ff85"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+      style={styles.linearGradient}>
+      <View style={styles.innerContainer}>
+        <Text style={styles.buttonText}>THIS GRADIENT BORDER</Text>
+      </View>
+    </LinearGradient>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  linearGradient: {
+    height: 150,
+    width: 600,
+    borderRadius: 20, // <-- Outer Border Radius
   },
-});
+  innerContainer: {
+    borderRadius: 15, // <-- Inner Border Radius
+    flex: 1,
+    margin: 5, // <-- Border Width
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: "Gill Sans",
+    fontWeight: "bold",
+    textAlign: "center",
+    margin: 10,
+    color: "#cc2b5e",
+    backgroundColor: "transparent",
+  },
+})
+
+export default APP
