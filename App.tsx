77c65fb9d398726fc44656b61bc7ace96b7aea9b
Peninah98 <peninangizwenayo@gmail.com>
2025-01-08 10:31:48 +0200

    feat: test platform module properties

--- App.tsx
+++ App.tsx
@@ -1,18 +1,33 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, Platform } from "react-native"
 import { LinearGradient } from "expo-linear-gradient"
 
 const APP = () => {
   return (
-    <LinearGradient
-      colors={["#bb52aa", "#63ff85"]}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 0 }}
-      style={styles.linearGradient}>
-      <View style={styles.innerContainer}>
-        <Text style={styles.buttonText}>THIS GRADIENT BORDER</Text>
-      </View>
-    </LinearGradient>
+    <View style={{ flex: 1, padding: 10, alignItems: "center" }}>
+      <LinearGradient
+        colors={["#bb52aa", "#63ff85"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        style={styles.linearGradient}>
+        <View style={styles.innerContainer}>
+          {Platform.OS === "web" ? (
+            <Text>The App is running on the web platform</Text>
+          ) : (
+            <Text>The App is running on the android platform</Text>
+          )}
+        </View>
+        <View>
+          <Text> {JSON.stringify(Platform.constants)}</Text>
+        </View>
+      </LinearGradient>
+      <Text style={{ alignItems: "center" }}>
+        Is this an iPad? {Platform.isPad ? "Yes" : "No"}
+      </Text>
+      <Text style={{ alignItems: "center" }}>
+        Is this an isTv? {Platform.isTV ? "Yes" : "No"}
+      </Text>
+    </View>
   )
 }
 
@@ -20,14 +35,18 @@ const styles = StyleSheet.create({
   linearGradient: {
     height: 150,
     width: 600,
-    borderRadius: 20, // <-- Outer Border Radius
+    borderRadius: 20,
+    alignSelf: "center",
+    justifyContent: "center",
+    marginVertical: 80,
   },
   innerContainer: {
-    borderRadius: 15, // <-- Inner Border Radius
+    borderRadius: 15,
     flex: 1,
-    margin: 5, // <-- Border Width
+    margin: 5,
     backgroundColor: "#fff",
     justifyContent: "center",
+    alignItems: "center",
   },
   buttonText: {
     fontSize: 18,
